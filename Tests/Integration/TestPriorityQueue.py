3a3ce5677f6671a0928daa42c57dcd7207fcac4d
80974397321 <32105251+80974397321@users.noreply.github.com>
2019-12-15 20:30:20 +0200

    Adding integration testing scripts

---
+++ Tests/Integration/TestPriorityQueue.py
@@ -0,0 +1,17 @@
+from utils.utils import PriorityQueueEvent, EventPriorityQueue
+from threading import Thread
+from multiprocessing import Queue
+from typing import List
+
+
+class ProducerThread(Thread):
+    def __init__(self, event_queue: 'PriorityQueueEvent', message_queue: Queue, events: List[EventPriorityQueue]):
+        Thread.__init__()
+        self.event_queue_ref = event_queue
+        self.message_queue_ref = message_queue
+        self.event_list = events
+
+    def run(self):
+        for event in self.events:
+            pass
+
